d4e91db23fc9ce7aaab6df8e3616fd4a6a7375d2
Ilya <ilyachimytov@gmail.com>
2020-05-17 08:35:10 +0800

    twig lint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,7 @@ cp /action/problem-matcher.json /github/workflow/problem-matcher.json
 
 echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/problem-matcher.json"
 
-${INPUT_TWIGLINT_BIN_PATH} --report=checkstyle ${INPUT_PATHS}
-~/.composer/vendor/friendsoftwig/twigcs/bin/twigcs /test/
+/root/.composer/vendor/friendsoftwig/twigcs/bin/twigcs ${INPUT_PATHS}
 status=$?
 
 echo "::remove-matcher owner=phpcs::"
