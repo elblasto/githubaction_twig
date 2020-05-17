673d4792f3715500be0ef99496395445afde8fd1
Ilya <ilyachimytov@gmail.com>
2020-05-17 09:02:13 +0800

    problem matcher

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ cp /action/problem-matcher.json /github/workflow/problem-matcher.json
 
 echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/problem-matcher.json"
 
-/root/.composer/vendor/friendsoftwig/twigcs/bin/twigcs ${INPUT_PATHS} --reporter checkstyle
+/root/.composer/vendor/friendsoftwig/twigcs/bin/twigcs ${INPUT_PATHS}
 status=$?
 
 echo "::remove-matcher owner=phpcs::"
